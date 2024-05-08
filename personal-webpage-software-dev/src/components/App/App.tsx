da30b60812cebaa3b9e1fbf04a7fce69777e6591
KibbaFlyer <kristoffer.flygare@icloud.com>
2024-05-08 14:15:50 +0200

    Adding Google Analytics and cookie banner

--- personal-webpage-software-dev/src/components/App/App.tsx
+++ personal-webpage-software-dev/src/components/App/App.tsx
@@ -6,19 +6,52 @@ import Education from '../Education/Education.tsx'
 import Experience from '../Experience/Experience.tsx'
 import Projects from '../Projects/Projects.tsx'
 import Contact from '../Contact/Contact.tsx'
+import CookieConsent, { Cookies } from "react-cookie-consent";
+import { useTranslation } from 'react-i18next';
+
 
 function App() {
+
+  const handleAcceptCookie = () => {
+    const script1 = document.createElement('script');
+    script1.src = "https://www.googletagmanager.com/gtag/js?id=G-KY8SZVYSLN";
+    script1.async = true;
+    document.head.appendChild(script1);
+
+    const script2 = document.createElement('script');
+    script2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-KY8SZVYSLN');
+    `;
+    document.head.appendChild(script2);
+  }
+
+  const handleDeclineCookie = () => {
+    Cookies.remove("_ga");
+    Cookies.remove("_ga_KY8SZVYSLN");
+  };
+
+  const { t } = useTranslation('App');
 
   return (
     <>
       <div className={appStyles.App}>
-        <Navbar/>
-        <Intro/>
-        <About/>
-        <Education/>
-        <Experience/>
-        <Projects/>
-        <Contact/>
+        <Navbar />
+        <Intro />
+        <About />
+        <Education />
+        <Experience />
+        <Projects />
+        <Contact />
+        <CookieConsent
+          enableDeclineButton
+          onAccept={handleAcceptCookie}
+          onDecline={handleDeclineCookie}
+        >
+          {t('cookieConsent')}
+        </CookieConsent>
       </div>
     </>
   )
